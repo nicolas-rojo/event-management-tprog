ce4486d23e2c2fc77f551450108571f763cba03b
Luca Scaboni <luca.scaboni@fing.edu.uy>
2025-09-12 16:19:53 -0300

    Verificar contraseñar

--- tarea2/register/script.js
+++ tarea2/register/script.js
@@ -2,15 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const tipoSelect = document.getElementById('tipo');
     const camposOrg = document.getElementById('Campos-Org');
     const camposAsist = document.getElementById('Campos-Asist');
-
+    const campoDesc = document.getElementById('desc');
+    const campoUrl = document.getElementById('url');
+    const campoApellido = document.getElementById('apellido');
+    const campoFecha = document.getElementById('fechaNac');
     
+    const form = document.getElementById('form');
+    const confirmarError = document.getElementById('confirmarError');
+    const password = document.getElementById('password');
+    const confirmacion = document.getElementById('confirmacion');
+
     function actualizarCampos() {
         if (tipoSelect.value === 'Organizador') {
             camposOrg.style.display = 'block';
             camposAsist.style.display = 'none';
+            campoDesc.value = "";
+            campoUrl.value = "";
+            campoApellido.value = 'default';
+            campoFecha.value = '0001-01-01';
+            
         } else if (tipoSelect.value === 'Asistente') {
             camposOrg.style.display = 'none';
             camposAsist.style.display = 'block';
+            campoDesc.value = 'default';
+            campoUrl.value = 'default';
+            campoApellido.value = "";
+            campoFecha.value = null;
         } else {
             camposOrg.style.display = 'none';
             camposAsist.style.display = 'none';
@@ -23,15 +40,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // Ejecuta al cargar la página para mostrar los campos
     actualizarCampos();
     
-    const form = document.querySelector('form');
-    const password = document.getElementById('password');
-    const confirmacion = document.getElementById('confirmacion');
-
     form.addEventListener('submit', (e) => {
+        e.preventDefault();
         if (password.value !== confirmacion.value) {
-            e.preventDefault();
-            alert('Las contraseñas no coinciden');
-            confirmacion.focus();
+            confirmarError.innerText = "Las contraseñas no coinciden";
+        } else {
+            confirmarError.innerText = "";
         }
     });
 });
