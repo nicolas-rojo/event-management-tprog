3f6703aced9c0c8743ede02127984efdeedc71e0
Luca Scaboni <luca.scaboni@fing.edu.uy>
2025-09-19 14:53:55 -0300

    Modificaciones Hardcodeo

---
+++ tarea2/notLogged/login/scriptConsultaDetalleOrg.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const form = document.querySelector("form");
+
+	const usuarioInput = document.getElementById("usuario");
+	const passInput = document.getElementById("clave");
+	const confirmarError = document.getElementById('confirmarError');
+
+	form.addEventListener("submit", (e) => {
+		e.preventDefault(); // Evita que el formulario se envíe normalmente
+
+		const nickmail = usuarioInput.value.trim();
+		const clave = passInput.value.trim();
+		const user = checkLogin(nickmail, clave);
+		if (user) {
+			confirmarError.innerText = "";
+			if (user.nickname == "atorres") {
+				window.location.href = "../../loggedAsist/consultaUsuario/consultaDetalleOrganizador.html";
+			} else {
+				window.location.href = "../../loggedOrg/consultaUsuario/consultaDetalleOrganizador.html";
+			}
+		} else {
+			confirmarError.innerText = "Datos Incorrectos";
+		}
+	});
+});
+
+function checkLogin(nickmail, clave) {
+	const usuarios = [
+		{nickname: "atorres", mail: "atorres@gmail.com", pass: "123.torres", role: "asistente", img: "../src/images/IMG-US01.jpg"}, 
+		{nickname: "miseventos", mail: "contacto@miseventos.com", pass: "22miseventos", role: "organizador", img: "../src/images/IMG-US04.jpeg"}]
+	return (usuarios.find(u => (u.nickname === nickmail || u.mail === nickmail) && u.pass === clave) || null);
+}
