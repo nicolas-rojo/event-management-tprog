9f221ce28244462c9d82a3fbbbe005b8e6a32658
Luca Scaboni Morales - ENS <luca.scaboni@pcunix136.fing.edu.uy>
2025-11-07 19:44:29 -0300

    otros arreglos de archivos

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-MAVEN_CMD="$SCRIPT_DIR/apache-maven-3.9.11/bin/mvn"
+MAVEN_CMD="$SCRIPT_DIR/../apache-maven-3.9.11/bin/mvn"
 
 cd "$SCRIPT_DIR/tarea1"
 "$MAVEN_CMD" clean package
